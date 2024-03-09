9efee166dcec49765f9fc2a4bcfb70c653ac4eb8
nathaneckel <nathan@patientpaperwork.com>
2024-03-09 14:40:20 -0500

    progress

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -1,30 +1,32 @@
 package com.zipcodewilmington;
-
 /**
  * Created by leon on 1/24/18.
  */
 public class PersonHandler {
     private final Person[] personArray;
-
     public PersonHandler(Person[] personArray) {
         this.personArray = personArray;
     }
 
-    // NOTICE how the comments can be used to step-by-step break down the problem into 
-    // simple lines of code...
-
-    //will do. love it.
-    
-    public String whileLoop() {
-        String result = "";
+    // NOTICE how the comments can step-by-step break the problem into simple lines of code...
+    public static void main(String[] args) {
         // create a `counter`
+        int counter = 0;
         // while `counter` is less than length of array
+        int counter = personArray.length;
+        while (boolean counter < personArray.length; counter++) {
+
+        }
             // begin loop
-
                 // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
 
+        for (int i = 0; i < currentPerson; personArray[counter];
+                // get `string Representation` of `currentPerson`
+        currentPerson[]
+                // append `stringRepresentation` to `result` variable
+                return new StringBuilder()
+                        .append(stringRepresentation.result)
+                        .toString()
             // end loop
         return result;
     }
@@ -49,12 +51,14 @@ public class PersonHandler {
 
 
 
-    public String forEachLoop() {
-        String result = "";
+    public static void String (forEachLoop[]
+            {
         // identify array's type
         // identify array's variable-name
+        String result = "";
 
         // use the above discoveries to declare for-each-loop signature
+        for <result[]
             // begin loop
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
